f9810d4a9cd3a829dca0eb38e0aee3d084f27f94
aiwei <aiwei@jd.com>
2023-01-16 11:24:11 +0800

    init

--- AddOns/Aph/Locales/enUS.lua
+++
@@ -1,7 +0,0 @@
-﻿local L = LibStub("AceLocale-3.0"):NewLocale("APH", "enUS", true)
- 
-L["Empowering"] = true
-L["Use Grants (%d+)"] = true
-L["(%d+) [.*]?Artifact"] = true
-L["Use Grants ([0-9%.]+)[ ]?(.-) Artifact Power"] = true
-L["UNIT_million"] = 1000000
--- AddOns/Aph/Locales/zhCN.lua
+++
@@ -1,9 +0,0 @@
-﻿local L = LibStub("AceLocale-3.0"):NewLocale( "APH", "zhCN" )
-
-if not L then return end
-
-L["Empowering"] = "强化"
-L["Use Grants (%d+)"] = "将(%d+)"
-L["Use Grants ([0-9%.]+)[ ]?(.-) Artifact Power"] = "将([0-9%.]+)[ ]?(.-)点神器"
-L["UNIT_万"] = 10000
-L["UNIT_亿"] = 100000000
